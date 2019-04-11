6843dab88ff76adde7422914857f8d170e4c8960
Ismael Dabo <ismaeldabo@MacBook-Pro-de-Ismael.local>
2019-04-11 15:16:55 +0200

    New API récuperer session

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,56 +4,65 @@ let mailer = require('../../modules/mailer');
 
 module.exports = {
 	post: async function (req, res) {
-	let user = new User();
-	let result;
-	let msg;
-	let error;
-	let {email, firstname, lastname, password, hash} = req.body;
-	switch (req.params.param) {
-		case 'register':
-			let newHash = sha256("L6Y&JsfJ#KURxuRj" + email + Date.now());
-			result = await user.register(email, firstname, lastname, password, newHash);
-			if (result) {
-				await mailer.sendRegisterConfirmationLink(email, newHash);
-				console.log("send " + email);
-				res.set("Content-Type", "application/json");
-				res.send({ msg: result});
-			}else res.status(401).send(error);
-			break;
-		case 'confirm':
-			//return true or false
-			result = await user.validateUser(hash);
-			if(result){
-				res.set("Content-Type", "application/json");
-				res.send({ msg: "User confirmed"});
-			}else res.status(401).send("Error user non validated");
-			break;
-		case 'login':
-			//return les infos de l'user : session ?
-			result = await user.login(email, password);
-			if(result){
-				req.session.uid = 5;
-
-				res.set("Content-Type", "application/json");
-				res.send({ msg: "User logged"});
-				console.log(result);
-				// res.session.uid = result.id;
-			}else res.status(400).send("Error login");
-			break;
-		default:
-			result = {};
-			break;
+		let user = new User();
+		let result;
+		let msg;
+		let error;
+		let { email, firstname, lastname, password, hash } = req.body;
+		switch (req.params.param) {
+			case 'register':
+				let newHash = sha256("L6Y&JsfJ#KURxuRj" + email + Date.now());
+				result = await user.register(email, firstname, lastname, password, newHash);
+				if (result) {
+					await mailer.sendRegisterConfirmationLink(email, newHash);
+					console.log("send " + email);
+					res.set("Content-Type", "application/json");
+					res.send({ msg: result });
+				} else res.status(401).send(error);
+				break;
+			case 'confirm':
+				//return true or false
+				result = await user.validateUser(hash);
+				if (result) {
+					res.set("Content-Type", "application/json");
+					res.send({ msg: "User confirmed" });
+				} else res.status(401).send("Error user non validated");
+				break;
+			case 'login':
+				//return les infos de l'user : session ?
+				result = await user.login(email, password);
+				if (result) {
+					console.log(result);
+					req.session.uid = result.id;
+					req.session.firstname = result.firstname;
+					req.session.lastname = result.lastname;
+					req.session.email = result.email;
+					res.set("Content-Type", "application/json");
+					res.send({ msg: "User logged" });
+					// res.session.uid = result.id;
+				} else res.status(400).send("Error login");
+				break;
+			default:
+				result = {};
+				break;
 		}
 	},
 	get: async function (req, res) {
-	let user = new User();
-	let result;
-	switch (req.params.param) {
-		case 'all':
-		if (!req.query.user_id)
-				result = await user.getUsers();
-			else result = await user.getUserById(req.query.user_id);
-			break;
+		let user = new User();
+		let result;
+		switch (req.params.param) {
+			case 'all':
+				if (!req.query.user_id)
+					result = await user.getUsers();
+				else result = await user.getUserById(req.query.user_id);
+				break;
+			case 'session':
+				if (req.session.uid) {
+					result = await user.getUserById(req.session.uid);
+				}
+				else
+					result = false;
+				break;
 		}
 		if (result) {
 			res.set("Content-Type", "application/json");
