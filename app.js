0ec5b3d14d2c71e1bf3ea7953fc8fe8a14c1a35b
kmesbah <kmesbah@accengage.com>
2019-04-11 15:40:04 +0200

    indentation fix

--- app.js
+++ app.js
@@ -4,27 +4,23 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let session = require('express-session');
-
 let app = express();
-
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(session({ secret: 'beclou'}));
-app.use(session({ uid: 1}));
+app.use(session({secret: 'beclou'}));
+app.use(session({uid: 1}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set les routes public views
 // Pour ajouter un chemin vers un .html, copier coller la fonction ci-dessous et changer le nom du ficher
 app.get('/', function (req, res) {
-    if(!req.session.uid) {
+    if (!req.session.uid) {
         res.redirect('/login');
         // res.sendFile(path.join(__dirname + '/public/views/login.html'));
-    }
-    else
+    } else
         res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
@@ -33,7 +29,6 @@ app.get('/signin', function (req, res) {
 });
 
 app.get('/login', function (req, res) {
-
     res.sendFile(path.join(__dirname + '/public/views/login.html'));
 });
 
